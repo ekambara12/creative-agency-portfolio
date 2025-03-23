b9313ee91e5cf9c86e10d7e4af5664fce3cbd07b
Omnath <ekambara@ualberta.ca>
2025-03-22 20:00:02 -0400

    Setup GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/creative-agency-portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/creative-agency-portfolio/' : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+}
+
+module.exports = nextConfig 
